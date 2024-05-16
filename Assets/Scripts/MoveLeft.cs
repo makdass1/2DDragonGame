18107f6d96e499742eb3387f2f2b253bebfbc13b
Mustafa Akdaş <makdas163@gmail.com>
2024-05-16 13:47:23 +0300

    Enemies and Moving Completed

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,9 +9,30 @@ public class MoveLeft : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * Speed );
+        transform.Translate(Vector3.left * Time.deltaTime * Speed , Space.World) ;
 
-        if (transform.position.x < -15)
+        if (transform.position.x < -24)
+        {
             transform.position += Vector3.right * 30;
+            ShowRandomSprite();
+
+        }
     }
+    private void ShowRandomSprite()
+    {
+
+        int index = UnityEngine.Random.Range(0, 3);
+        int childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            bool shouldShow = index == i;
+            child.gameObject.SetActive(shouldShow);
+        }
+    }
+    private void OnEnable()
+    {
+        ShowRandomSprite();
+    }
+
 }
